f7bee4d71eb348b024ff038ff5327df32adc30c1
chuck francis <chuck_francis10@yahoo.com>
2020-08-20 19:12:23 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SplittingASentenceIntoIndividualWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s = "I like cheese. Lots!";
+
+            char[] delimiters = { ',', '!', '.', ' ' };
+            // split on all the delimiters and remove any empty ones
+            string[] words = s.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            // loop through all the elements
+            for (int i = 0; i < words.Length; i++)
+            {
+                // print the word at element index i
+                Console.WriteLine(words[i]);
+            }
+        }
+    }
+}
